175409fa37938383bc84923b5aab45cf5d633842
yang960701 <yang960701@gmail.com>
2022-05-09 15:39:37 +0900

    testYang

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 뭘봐
+          Edit <code>src/App.js</code> 뭘봐 ggk
         </p>
         <a
           className="App-link"
