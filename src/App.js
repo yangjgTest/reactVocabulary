61b9b90bfc3303ef0a549e0c5d0181c0766a01a0
yang960701 <yang960701@gmail.com>
2022-05-09 17:03:10 +0900

    tag

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@
 import './App.css';
 
 function App() {
+  const name = "Tom";
+  const naver  = {
+    name : '네이버',
+    url : 'https://www.naver.com'
+  }
   return (
     <div className="App">
      <h1 style={{
-       color : 'red'
+       color : 'red',
+       backgroundColor : 'green'
      }} >
-        welcome</h1>
+        welcome
+        <h1>히히</h1>
+        </h1>
+        <a href={naver.url}>{naver.name}</a>
+        
     </div>
   );
 }
