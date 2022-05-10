55b702b2a21bdcde2804514929fd530dc1329501
yang960701 <yang960701@gmail.com>
2022-05-10 14:17:50 +0900

    import style CSS

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 import Hello from './component/Hello';
 import Welcome from './component/Welcome';
+import style from './App.module.css'
 
 function App() {
   return (
     <div className="App">
-      <Hello/>
-      <Welcome/>
+      <Hello/>  
+      <div className={style.box}>App</div>
     </div>
+
+  
   );
 }
 
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -3,15 +3,24 @@
 // }
 
 import World from "./World";
+import styles from "./Hello.module.css"
 
 // export default Hello;
 
 export default function Hello(){
     return (
     <div>
-        <p>Hello</p>
-        <World/>
-        <World/>
+        <p style={
+{
+    color : '#f00',
+    borderRight : '12px solid #000',
+    marginBottom : '50px',
+    opacity : 0.5,
+    
+}
+        }>Hello</p>
+        <div className={styles.box}>Hello</div>
+    
     </div>
      );
 }
