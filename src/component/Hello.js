b208524a53c8f359eea109afd0378f9fa431225f
yang960701 <yang960701@gmail.com>
2022-05-12 16:59:12 +0900

    changeNmae

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -4,14 +4,24 @@
 
 import World from "./World";
 import styles from "./Hello.module.css"
+import { useState } from "react";
 
 // export default Hello;
 
 export default function Hello(){
+    // let name ="Mike";
+    const [name, setName] = useState("Mike");
+
+    function changeName(){
+        const newName = name == "Mike" ? "Jame" : "Mike";
+        setName(newName)
+          
+    }
     return (
     <div>
         <h1>state</h1>
-        <h2>컴포넌트의 속성값</h2>
+        <h2 id = "name">{name}</h2>
+        <button onClick={changeName}>changeName</button> 
     </div>
      );
 }
