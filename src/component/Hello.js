814f7fd8317d8b2ccd13aebef978b5e37e8fdaca
yang960701 <yang960701@gmail.com>
2022-05-17 17:40:46 +0900

    profs 전달

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -5,27 +5,31 @@
 import World from "./World";
 import styles from "./Hello.module.css"
 import { useState } from "react";
+import UserName from "./UserName";
 
 // export default Hello;
 
-export default function Hello(profs){
+export default function Hello({age}){
     // let name ="Mike";
-    console.log(profs)
+
     const [name, setName] = useState("Mike");
-    const [age, setAge] = useState(profs.age)
     
+    const msg = age > 19 ? "성인입니다." : "미성년자입니다.";
+
     function changeName(){
         const newName = name == "Mike" ? "Jame" : "Mike";
-        setName(newName)
+        setName(newName) 
           
     }
     return (
     <div>
         <h1>state</h1>
-        <h2 id = "name">{name}({age})</h2>
+        <h2 id = "name">{name}({age}) : {msg}</h2> 
+
+        <UserName name ={name} />
         <button onClick={() => {
             setName(name === "Mike" ? "Jame" : "Mike");
-            setAge(age + 1);    
+           
             console.log(age)
         }}>changeName</button> 
     </div>
