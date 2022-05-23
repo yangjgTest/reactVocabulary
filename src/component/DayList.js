1a6fc1ec43b2e0929829a8e919a88159eaeac678
yang960701 <yang960701@gmail.com>
2022-05-23 16:29:41 +0900

    useeffect

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,17 +1,59 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json'
+
 
 export default function DayList() {
-    console.log(dummy);
-    return <ul className='list_day'>
-        {dummy.days.map(day => (
-            <li key={day.id}>  
-                <Link to={`/day/${day.day}`}>
-                Day {day.day}
-                </Link> 
-            </li>
-        ))}
-        {/* `day/${day.day}` */}
+   
+   
 
-    </ul>;
+    const [days,SetDays] = useState([]);
+    const [count,setCount] = useState(0);
+
+    function onclick(){
+         setCount(count+1);
+    }
+
+    function onclick2(){
+
+
+       SetDays([
+           ...days,
+           {
+               id:Math.random(),
+               day : 1
+           }
+       ])
+
+
+
+   }
+
+    useEffect(()=>{
+        console.log("Count change");
+    },[]);
+
+    return (
+        <>
+        <ul className='list_day'>
+
+           {days.map(day => (
+                <li key={day.id}>  
+
+
+                    <Link to={`/day/${day.day}`}>
+                    Day {day.day}
+                    </Link> 
+
+
+                </li>
+            ))}
+            {/* `day/${day.day}` */}
+    
+        </ul>
+        <button onClick={onclick}>{count}</button>
+        <button onClick={onclick2}>DayChange</button>
+        </>
+    )
+   
+        
 }       
